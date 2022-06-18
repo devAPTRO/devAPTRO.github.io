98b3791f088a5446caf5bfce56aeb1994f43de28
devAPTRO <coupdequeuelol@gmail.com>
2022-06-18 20:53:09 +0200

    Added "patouick.omg"

---
+++ data/patouick.omg/patouick.omg.bat
@@ -0,0 +1,34 @@
+@echo off
+echo patouick a le cancer du cul>>patouick.omg
+echo patouick a le cancer du cul>>patouick.omgg
+echo patouick a le cancer du cul>>patouick.omggg
+echo patouick a le cancer du cul>>patouick.omgggg
+echo patouick a le cancer du cul>>patouick.omggggg
+echo patouick a le cancer du cul>>patouick.omgggggg
+echo patouick a le cancer du cul>>patouick.omggggggg
+echo patouick a le cancer du cul>>patouick.omgggggggg
+echo patouick a le cancer du cul>>patouick.omggggggggg
+echo patouick a le cancer du cul>>patouick.omgggggggggg
+echo patouick a le cancer du cul>>patouick.omggggggggggg
+echo patouick a le cancer du cul>>patouick.omgggggggggggg
+echo patouick a le cancer du cul>>patouick.omggggggggggggg
+echo patouick a le cancer du cul>>patouick.omgggggggggggggg
+echo patouick a le cancer du cul>>patouick.omggggggggggggggg
+echo patouick a le cancer du cul>>patouick.omgggggggggggggggg
+echo patouick a le cancer du cul>>patouick.omggggggggggggggggg
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
+start ss.html
